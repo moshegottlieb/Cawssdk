725f46f5e7dfdd93fee3091651e9951e6cf05c89
Moshe Gottlieb <moshe@sharkfood.com>
2017-11-16 11:17:13 +0200

    Swift package support for swift 4

--- Package.swift
+++ Package.swift
@@ -5,8 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "Cawssdk",
+    products: [
+        .library(
+            name: "Cawssdk",
+            targets: ["Cawssdk"]),
+        ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ]
+    ],
+    targets: [
+        .target(
+            name: "Cawssdk",
+            dependencies: []),
+        ]
 )
