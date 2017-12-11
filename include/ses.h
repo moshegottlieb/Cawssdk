aaf87afb6a9ffd6203b551a11c3acbf3b722c0cf
Moshe Gottlieb <moshe@sharkfood.com>
2017-12-11 18:15:52 +0200

    to list is nullable instead of nonnull

--- include/ses.h
+++ include/ses.h
@@ -7,7 +7,7 @@ extern "C" {
 
 AWSObjectRef PIM_NONNULL SESCreate(const char* PIM_NONNULL region,const char* PIM_NONNULL accessKeyId,const char* PIM_NONNULL secretKey,AWSResult* PIM_NONNULL result);
 void SESDestroy(AWSObjectRef PIM_NONNULL object);
-AWSErrorPolicy SESSendEmail(AWSObjectRef PIM_NONNULL object, const char* PIM_NONNULL from, const char* PIM_NONNULL * PIM_NONNULL to,int toCount,const char* PIM_NONNULL * PIM_NULLABLE cc,int ccCount,const char* PIM_NONNULL * PIM_NULLABLE bcc,int bccCount,const char* PIM_NONNULL subject,const char* PIM_NONNULL body,int isHtml,AWSResult* PIM_NONNULL result);
+AWSErrorPolicy SESSendEmail(AWSObjectRef PIM_NONNULL object, const char* PIM_NONNULL from, const char* PIM_NONNULL * PIM_NULLABLE to,int toCount,const char* PIM_NONNULL * PIM_NULLABLE cc,int ccCount,const char* PIM_NONNULL * PIM_NULLABLE bcc,int bccCount,const char* PIM_NONNULL subject,const char* PIM_NONNULL body,int isHtml,AWSResult* PIM_NONNULL result);
 
 #ifdef __cplusplus
 }
